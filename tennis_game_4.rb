c952ddad1ee7745d79aff7c3a2c105b776accb55
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2023-03-02 15:09:57 +0900

    Add pattern match game

--- tennis_game_4.rb
+++ tennis_game_4.rb
@@ -1,121 +1,44 @@
+# frozen_string_literal: true
+
 class TennisGame4
-  def initialize(player1_name, player2_name)
-    @player1_name = player1_name
-    @player2_name = player2_name
+  ALL = "All"
+  LOVE = "Love"
+  DEUCE = "Deuce"
+  SCORE_DICT = {
+    0 => LOVE,
+    1 => "Fifteen",
+    2 => "Thirty",
+    3 => "Forty",
+  }.freeze
+
+  def initialize player1, player2
+    @player1 = player1
+    @player2 = player2
     @p1points = 0
     @p2points = 0
   end
 
-  def won_point(player_name)
-    if player_name == @player1_name
-      p1Score()
+  def won_point player_name
+    if player_name == player1
+      p1Score
     else
-      p2Score()
+      p2Score
     end
   end
 
   def score
-    result = ""
-    if (@p1points == @p2points and @p1points < 3)
-      if (@p1points==0)
-        result = "Love"
-      end
-      if (@p1points==1)
-        result = "Fifteen"
-      end
-      if (@p1points==2)
-        result = "Thirty"
-      end
-      result += "-All"
-    end
-    if (@p1points==@p2points and @p1points>2)
-        result = "Deuce"
-    end
-
-    p1res = ""
-    p2res = ""
-    if (@p1points > 0 and @p2points==0)
-      if (@p1points==1)
-        p1res = "Fifteen"
-      end
-      if (@p1points==2)
-        p1res = "Thirty"
-      end
-      if (@p1points==3)
-        p1res = "Forty"
-      end
-      p2res = "Love"
-      result = p1res + "-" + p2res
-    end
-    if (@p2points > 0 and @p1points==0)
-      if (@p2points==1)
-        p2res = "Fifteen"
-      end
-      if (@p2points==2)
-        p2res = "Thirty"
-      end
-      if (@p2points==3)
-        p2res = "Forty"
-      end
-
-      p1res = "Love"
-      result = p1res + "-" + p2res
-    end
-
-    if (@p1points>@p2points and @p1points < 4)
-      if (@p1points==2)
-        p1res="Thirty"
-      end
-      if (@p1points==3)
-        p1res="Forty"
-      end
-      if (@p2points==1)
-        p2res="Fifteen"
-      end
-      if (@p2points==2)
-        p2res="Thirty"
-      end
-      result = p1res + "-" + p2res
-    end
-    if (@p2points>@p1points and @p2points < 4)
-      if (@p2points==2)
-        p2res="Thirty"
-      end
-      if (@p2points==3)
-        p2res="Forty"
-      end
-      if (@p1points==1)
-        p1res="Fifteen"
-      end
-      if (@p1points==2)
-        p1res="Thirty"
-      end
-      result = p1res + "-" + p2res
-    end
-    if (@p1points > @p2points and @p2points >= 3)
-      result = "Advantage " + @player1_name
-    end
-    if (@p2points > @p1points and @p1points >= 3)
-      result = "Advantage " + @player2_name
-    end
-    if (@p1points>=4 and @p2points>=0 and (@p1points-@p2points)>=2)
-      result = "Win for " + @player1_name
-    end
-    if (@p2points>=4 and @p1points>=0 and (@p2points-@p1points)>=2)
-      result = "Win for " + @player2_name
-    end
-    result
-  end
-
-  def setp1Score(number)
-    (0..number).each do |i|
-        p1Score()
-    end
-  end
-
-  def setp2Score(number)
-    (0..number).each do |i|
-      p2Score()
+    case [@p1points, @p2points]
+    in points1, ^points1 if points1 <= 2 then combine_score SCORE_DICT[points1], ALL
+    in points1, ^points1 then DEUCE
+    in 0, points2 if points2 <= 3 then combine_score LOVE, SCORE_DICT[points2]
+    in points1, 0 if points1 <= 3 then combine_score SCORE_DICT[points1], LOVE
+    in points1, points2 if points1 <= 3 and points2 <= 3 then combine_score SCORE_DICT[points1], SCORE_DICT[points2]
+    in points1, points2 if points1 - points2 >= 2 then win_for player1
+    in points1, points2 if points2 - points1 >= 2 then win_for player2
+    in points1, points2 if points1 - points2 == 1 then advantage player1
+    in points1, points2 if points2 - points1 == 1 then advantage player2
+    else
+      "no match"
     end
   end
 
@@ -125,5 +48,21 @@ class TennisGame4
 
   def p2Score
     @p2points +=1
+  end
+
+  private
+
+  attr_reader :player1, :player2
+
+  def combine_score score1, score2
+    [score1,score2].join "-"
+  end
+
+  def win_for player
+    "Win for #{player}"
+  end
+
+  def advantage player
+    "Advantage #{player}"
   end
 end
